689ba35a36a1f68561720e2bdca43f5153dbaf8c
XiaoLFeng <gm@xiaolfeng.cn>
2021-02-16 12:19:40 +0800

    Upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm787251967-ec0a5330d2c19e6da040621e', //筱锋主页
+	'm784729736-d60d04750f27215f3486c03b', //筱锋博客
+	'm785863210-8d50642c34e80d1a3852a59b', //筱锋论坛
+	'm784730227-e8b66587dc76b89a75154bd1', //云端状态监控
+	'm784730155-a619bd1635c8822674caf1b8', //筱锋搜索主页
+	'm784730216-16ebb3be4fda7c3d5df17138', //筱锋云端记事本
+	'm785804335-c45ccef2a594519ded716a09', //API总服务
+	'm785956755-0b6438801e04732ca473d0df', //随机图片
+	'm784730129-8f9439b9e3e52e0a2f3d1886', //音乐下载站①
+	'm784730143-02f3a7e88bed5ab7611b5b1d', //音乐下载站②
+	'm785812487-a7232551a9ae3bbba5f7bb9a', //音乐解锁站
+	'm785860545-e0961c04049fa047be3c48b9', //谷歌镜像站
+];
+
+// refresh interval (in seconds)
+var __refresh = 120;
